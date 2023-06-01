8546e0d413712a7972c2d6d69f6417c262891cb2
boatengjulius14 <boatengjulius14@gmail.com>
2023-06-01 07:38:29 +0000

    Sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (!node || !node->parent)
 		return (NULL);
 
-	if (node->parent->left->n == node->n)
+	if (node->parent->left == node)
 		return (node->parent->right);
 	else
 		return (node->parent->left);
