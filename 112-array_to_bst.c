dc22629a4e6125abe9f85f0734b9b542ccf7b0f9
boatengjulius14 <boatengjulius14@gmail.com>
2023-06-02 03:06:48 +0000

    BST - Array to BST

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -13,7 +13,7 @@ bst_t *array_to_bst(int *array, size_t size)
 	bst_t *tree = NULL;
 	size_t i;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
